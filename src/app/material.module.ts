688865043582ac8f1fe4524f0f4fd3ac8d315f76
MoneshMS <moneshsanju@gmail.com>
2021-12-04 16:23:36 +0530

    added admin inputs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [MatIconModule],
-  exports: [MatIconModule],
+  imports: [MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatIconModule, MatFormFieldModule, MatInputModule],
 })
 export class MaterialModule {}
