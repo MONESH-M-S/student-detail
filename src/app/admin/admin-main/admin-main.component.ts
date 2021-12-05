424e9d56333349b8a536781a42747b72b1ad240f
MoneshMS <moneshsanju@gmail.com>
2021-12-05 06:18:26 +0530

    Added card-grid effect in admin main component

--- src/app/admin/admin-main/admin-main.component.ts
+++ src/app/admin/admin-main/admin-main.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-admin-main',
   templateUrl: './admin-main.component.html',
-  styleUrls: ['./admin-main.component.scss']
+  styleUrls: ['./admin-main.component.scss'],
 })
 export class AdminMainComponent implements OnInit {
+  studentDetails = [
+    { id: 1, roll: '19EIR050', paper: 3, project: 3 },
+    { id: 2, roll: '19EIR054', paper: 1, project: 5 },
+    { id: 3, roll: '19EIR052', paper: 3, project: 3 },
+    { id: 4, roll: '19EIR053', paper: 5, project: 1 },
+    { id: 5, roll: '19EIR055', paper: 3, project: 4 },
+    { id: 6, roll: '19EIR050', paper: 3, project: 3 },
+    { id: 7, roll: '19EIR054', paper: 1, project: 5 },
+    { id: 8, roll: '19EIR052', paper: 3, project: 3 },
+    { id: 9, roll: '19EIR053', paper: 5, project: 1 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onCardClick(id: number) {}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,25 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule],
-  exports: [MatIconModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+  ],
 })
 export class MaterialModule {}
