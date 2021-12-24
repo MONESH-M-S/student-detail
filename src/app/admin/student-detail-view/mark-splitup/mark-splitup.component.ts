8d5ab12b55ebe778801634095eab427c612af11f
MoneshMS <moneshsanju@gmail.com>
2021-12-24 14:58:11 +0530

    added to get output as excel from mark-splitup

--- src/app/admin/student-detail-view/mark-splitup/mark-splitup.component.ts
+++ src/app/admin/student-detail-view/mark-splitup/mark-splitup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-mark-splitup',
@@ -7,11 +8,41 @@ import { Location } from '@angular/common';
   styleUrls: ['./mark-splitup.component.scss'],
 })
 export class MarkSplitupComponent implements OnInit {
+  details: any[] = [
+    { event: 'sample1', maxiMark: 55, mark: 6 },
+    { event: 'sample2', maxiMark: 35, mark: 32 },
+    { event: 'sample3', maxiMark: 56, mark: 33 },
+  ];
   constructor(private location: Location) {}
 
   ngOnInit(): void {}
 
   goBack() {
     this.location.back();
+  }
+
+  exportExcel() {
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(this.details);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, 'detail');
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE,
+    });
+    FileSaver.saveAs(
+      data,
+      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+    );
   }
 }
