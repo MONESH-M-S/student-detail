8d19120d7ed7c517cdfd9bd5f61893087a95c66f
MoneshMS <moneshsanju@gmail.com>
2022-01-18 20:49:26 +0530

    change env. backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  BACKEND_URL: 'http://localhost:3000/',
-  // BACKEND_URL: 'https://student-detail-sap.herokuapp.com/'
+  // BACKEND_URL: 'http://localhost:3000/',
+  BACKEND_URL: 'https://student-detail-sap.herokuapp.com/'
 };
 
 /*
